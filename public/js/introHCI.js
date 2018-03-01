1cd45cd6a3b0de564b672ea07a43fd1b820ad708
Lillian Lee <lzl003@ucsd.edu>
2018-03-01 10:42:56 -0800

    maps and click

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -8,16 +8,36 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
+
 function initializePage() {
 	console.log("Page ready");
- 	initCamera();
+ 	//initCamera();
  	initGestures();
  	initRSVPForm();
 }
 
+
+
+
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+	$(function(){
+	  $("div.box").bind("taphold", tapholdHandler);
+	  function tapholdHandler(event){
+	    $(event.target).addClass("taphold");
+	 	 }
+	 });
+
+	$(function(){
+		$(".judge-img").bind("taphold", tapholdHandler);
+
+		function tapholdHandler(event){
+			var targetIDPrefix=event.target.id;
+			console.log("got prefix:"+targetIDPrefix);
+			$("#"+targetIDPrefix+"-bio").show();
+		}
+	});
 }
 
 // init RSVP form submit listener
